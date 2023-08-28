4b9e45080af6c19fbc6c73f95159c03567b9fed5
TapasDash <tapasdash017@gmail.com>
2023-08-28 16:48:14 +0530

    severles

--- hello.js
+++ hello.js
@@ -4,6 +4,8 @@ module.exports.handler = async (event) => {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ message: "Hello From Tapas!", event }),
+    body: JSON.stringify({ message: "Hello From Tapas update!", event }),
   };
 };
+//If you want to deploy only single function inset of whole stack:
+// sls deploy function -f hello
